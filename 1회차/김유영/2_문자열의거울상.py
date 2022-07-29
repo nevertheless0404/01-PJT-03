65295a033107e0048bfd2e68c55cfcf3f5e7312b
YuYeong <108647801+nevertheless0404@users.noreply.github.com>
2022-07-29 18:03:17 +0900

    Update 2_문자열의거울상.py

--- 1회차/김유영/2_문자열의거울상.py
+++ 1회차/김유영/2_문자열의거울상.py
@@ -2,6 +2,7 @@ import sys
 
 sys.stdin = open("/Users/yuyeong/Desktop/알고리즘/01-PJT-03/1회차/김유영/_문자열의거울상.txt")
 T = int(input())
+# ex) 'bdppq' => 'pqqbd'
 # 문자열을 뒤집어서 풀기 
 for test_case in range(1, T+1):
     case = input()
